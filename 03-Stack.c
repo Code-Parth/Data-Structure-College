e5fbf10e669282e2b0ac94b2477c006c776ec1f6
Parth Parmar <84669955+Code-Parth@users.noreply.github.com>
2022-11-10 05:59:54 +0000

    Complete Stack

--- 03-Stack.c
+++ 03-Stack.c
@@ -7,53 +7,90 @@
 // EXIT()
 
 #include <stdio.h>
-#define N 3
 
-int s[N];
-
-void main()
+int stack[100], choice, n, top, x, i;
+void push(void);
+void pop(void);
+void display(void);
+int main()
 {
-    int x, y, Option;
-    while (1)
+    top = -1;
+    printf("\n Enter the size of STACK[MAX=100]:");
+    scanf("%d", &n);
+    printf("\n\t STACK OPERATIONS USING ARRAY");
+    printf("\n\t--------------------------------");
+    printf("\n\t 1.PUSH\n\t 2.POP\n\t 3.DISPLAY\n\t 4.EXIT");
+    do
     {
-        printf("\nSelect Option which you want to perform.");
-        printf("\n01.PUSH()");
-        printf("\n02.POP()");
-        printf("\n03.PEEP()");
-        printf("\n04.CHANGE()");
-        printf("\n05.DISPLAY()");
-        printf("\n06.EXIT()\n");
-        scanf("Select Option : %d",&Option);
-        switch (Option)
+        printf("\n Enter the Choice:");
+        scanf("%d", &choice);
+        switch (choice)
         {
         case 1:
-            printf("\nPUSH() Function using");
-            break;
-
-        case 2:
-            printf("\nPOP() Function using");
-            break;
-
-        case 3:
-            printf("\nPEEP() Function using");
-            break;
-
-        case 4:
-            printf("\nCHANGE() Function using");
-            break;
-
-        case 5:
-            printf("\nDISPLAY() Function using");
-            break;
-
-        case 6:
-            printf("\nEXIT() Function using");
-            exit(0);
-            break;
-
-        default:
-            printf("Invalid Option!");
+        {
+            push();
             break;
         }
+        case 2:
+        {
+            pop();
+            break;
+        }
+        case 3:
+        {
+            display();
+            break;
+        }
+        case 4:
+        {
+            printf("\n\t EXIT POINT ");
+            break;
+        }
+        default:
+        {
+            printf("\n\t Please Enter a Valid Choice(1/2/3/4)");
+        }
+        }
+    } while (choice != 4);
+    return 0;
+}
+void push()
+{
+    if (top >= n - 1)
+    {
+        printf("\n\tSTACK is over flow");
+    }
+    else
+    {
+        printf(" Enter a value to be pushed:");
+        scanf("%d", &x);
+        top++;
+        stack[top] = x;
+    }
+}
+void pop()
+{
+    if (top <= -1)
+    {
+        printf("\n\t Stack is under flow");
+    }
+    else
+    {
+        printf("\n\t The popped elements is %d", stack[top]);
+        top--;
+    }
+}
+void display()
+{
+    if (top >= 0)
+    {
+        printf("\n The elements in STACK \n");
+        for (i = top; i >= 0; i--)
+            printf("\n%d", stack[i]);
+        printf("\n Press Next Choice");
+    }
+    else
+    {
+        printf("\n The STACK is empty");
     }
 }
